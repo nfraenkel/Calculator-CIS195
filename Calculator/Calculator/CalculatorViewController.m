d2912f3d9620133b0f63a82fec3e0954ae0affbd
Nathan Fraenkel <nfraenkel18@gmail.com>
2012-09-28 21:29:25 -0400

    got operators to work, clear button, backspace. still need to fix equal sign

--- Calculator/Calculator/CalculatorViewController.m
+++ Calculator/Calculator/CalculatorViewController.m
@@ -16,18 +16,25 @@
 
 @synthesize display;
 
-float total, other;
-NSString* operation;
+float runningTotal, numberOnScreen;
+NSString *operation, *nextOperation;
+BOOL operatorButtonWasJustPressed;
 
 
 - (IBAction)numberPressed:(UIButton *)sender {
     NSString *digit = [sender currentTitle];
     NSString *currentDisplay = display.text;
-    if ([currentDisplay isEqualToString:@"0"])
+    if ([currentDisplay isEqualToString:@"0"] || operatorButtonWasJustPressed){
         self.display.text = digit;
-    else
+        operatorButtonWasJustPressed = NO;
+    }
+    else {
         self.display.text = [currentDisplay stringByAppendingString:digit];
+        numberOnScreen = [display.text floatValue]; 
+
+    }
 }
+
 - (IBAction)dotPressed:(UIButton *)sender {
     NSString *currentDisplay = display.text;
     BOOL hasPeriodAlready = FALSE;
@@ -43,28 +50,86 @@ NSString* operation;
         display.text = [currentDisplay stringByAppendingString:@"."];
 }
 - (IBAction)operatorPressed:(UIButton *)sender {
-    operation = [sender currentTitle];
+    nextOperation = [sender currentTitle];
+    operatorButtonWasJustPressed = YES;
+    if ([operation isEqualToString:@""]){
+        runningTotal = [display.text floatValue];
+    }
+    if ([operation isEqualToString:@"+"]){
+        runningTotal += numberOnScreen;
+    }
+    if ([operation isEqualToString:@"-"]){
+        runningTotal -= numberOnScreen;
+    }
+    if ([operation isEqualToString:@"x"]){
+        runningTotal *= numberOnScreen;
+    }
+    if([operation isEqualToString:@"/"]){
+        if (numberOnScreen == 0.0){
+            display.text = @"Err";
+            runningTotal = 0;
+            numberOnScreen = 0;
+        }
+        else
+            runningTotal /= numberOnScreen;
+    }
+    display.text = [NSString stringWithFormat:@"%f", runningTotal];
     
+    // EQUAL button was pressed
+    if ([nextOperation isEqualToString:@"="]){
+//        numberOnScreen = 0;
+//        runningTotal = 0;
+//        operation = @"";
+    }
+    else {
+        operation = nextOperation;
+    }
     
 }
 - (IBAction)equalPressed:(UIButton *)sender {
+    operatorButtonWasJustPressed = YES;
+    display.text = [NSString stringWithFormat:@"%f", runningTotal];
+    numberOnScreen = 0;
+    runningTotal = 0;
+    operation = @"";
     
 }
 
-- (IBAction)clearPressed:(UIButton *)sender {
+- (IBAction)backSpacePressed:(UIButton *)sender {
+    if (display.text.length > 1){
+        display.text = [display.text substringToIndex:display.text.length - 1];
+    }
+    else {
+        display.text = @"0";
+    }
+    numberOnScreen = [display.text floatValue];
+}
+
+- (IBAction)clearAllPressed:(UIButton *)sender {
     display.text = @"0";
+    numberOnScreen = 0;
+    runningTotal = 0;
+    operation = @"";
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    runningTotal = 0;
+    numberOnScreen = 0;
+    operation = @"";
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)dealloc {
+
+    
 }
 
 @end
